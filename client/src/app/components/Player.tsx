1c0d85f7df95ff7581df3f92253b0924da80f704
AHW214 <adamweider98@gmail.com>
2024-09-24 18:14:07 -0700

    wip4

--- client/src/app/components/Player.tsx
+++ client/src/app/components/Player.tsx
@@ -8,25 +8,39 @@ export type Props = {
 };
 
 export default function Player({ hint, id, isTyping, name }: Props) {
-  const { cursorClass, onClick } =
+  const { classes, onClick } =
     hint !== undefined && hint !== ""
-      ? { cursorClass: "cursor-pointer", onClick: () => alert("contact!") }
-      : { cursorClass: "auto", onClick: () => {} };
+      ? {
+          classes: { cursor: "cursor-pointer", visibility: "visible" },
+          onClick: () => alert("contact!"),
+        }
+      : {
+          classes: { cursor: "auto", visibility: "invisible" },
+          onClick: () => {},
+        };
 
   return (
-    <div
-      key={id}
-      className={`pl-0 pt-0 min-w-40 max-w-72 min-h-16 max-h-48 p-2 border-2 border-zinc-300 rounded-lg ${cursorClass}`}
-      onClick={onClick}
-    >
-      <div className="w-fit -mx-[.125rem] -mt-[.125rem] px-2 border-2 rounded-tl-lg rounded-br-lg border-zinc-300">
-        <h3>{name}</h3>
+    <div key={id} className="relative group">
+      <div
+        className={`absolute top-0 left-0 w-full h-full flex items-center justify-center rounded-lg bg-zinc-800 invisible group-hover:${classes.visibility}`}
+      >
+        <h3 className="text-zinc-100 text-center text-4xl uppercase">
+          contact
+        </h3>
       </div>
-      {hint !== undefined && hint !== "" ? (
-        <p className="ml-2">{hint}</p>
-      ) : isTyping ? (
-        <p className="ml-2 tracking-widest">...</p>
-      ) : undefined}
+      <div
+        className={`pl-0 pt-0 p-2 min-w-40 max-w-72 min-h-16 max-h-48 border-2 border-zinc-300 rounded-lg ${classes.cursor}`}
+        onClick={onClick}
+      >
+        <div className="w-fit -mx-[.125rem] -mt-[.125rem] px-2 border-2 rounded-tl-lg rounded-br-lg border-zinc-300">
+          <h3>{name}</h3>
+        </div>
+        {hint !== undefined && hint !== "" ? (
+          <p className="ml-2">{hint}</p>
+        ) : isTyping ? (
+          <p className="ml-2 tracking-widest">...</p>
+        ) : undefined}
+      </div>
     </div>
   );
 }
